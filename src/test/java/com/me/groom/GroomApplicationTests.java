fa9008547057bba2a7bebe5e7896736311cacf43
Subham Gupta <subhamgupta@microsoft.com>
2021-12-20 17:32:19 +0530

    first commit

---
+++ src/test/java/com/me/groom/GroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.groom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
